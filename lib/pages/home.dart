83190294c427646482f3171bf3da2e49d0143178
fantao <phantasy015@qq.com>
2023-08-28 14:22:23 +0800

    change homeHeaderGallery and disable useless widget
    Signed-off-by: fantao <phantasy015@qq.com>

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -210,6 +210,7 @@ class HomePage extends StatelessWidget {
             ),
             */
             const SizedBox(height: 81),
+            /*
             _DesktopHomeItem(
               child: Row(
                 children: [
@@ -253,6 +254,7 @@ class HomePage extends StatelessWidget {
                 ],
               ),
             ),
+            */
             const SizedBox(height: 109),
           ],
         ),
