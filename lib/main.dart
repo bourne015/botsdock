ec2393da21b83e8576614ef076744103ed1649de
fantao <phantasy015@qq.com>
2024-03-21 07:00:13 +0800

    enable dark theme mode in home screen
    Signed-off-by: fantao <phantasy015@qq.com>

--- lib/main.dart
+++ lib/main.dart
@@ -44,7 +44,7 @@ class GalleryApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return ModelBinding(
       initialModel: GalleryOptions(
-        themeMode: ThemeMode.light,
+        themeMode: ThemeMode.system,
         textScaleFactor: systemTextScaleFactorOption,
         customTextDirection: CustomTextDirection.localeBased,
         locale: null,
