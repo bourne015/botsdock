584055f48fd57f42e16561a36d0f02d1096fb6ba
fantao <phantasy015@qq.com>
2025-04-18 16:39:13 +0800

    update model description
    Signed-off-by: fantao <phantasy015@qq.com>

--- lib/apps/chat/views/menu/settings_view.dart
+++ lib/apps/chat/views/menu/settings_view.dart
@@ -324,93 +324,37 @@ class SettingsViewState extends State<SettingsView> with RestorationMixin {
               ),
               horizontalMargin: 5,
               columnSpacing: 15,
+              headingTextStyle: Theme.of(context).textTheme.bodyMedium,
+              dataTextStyle: Theme.of(context).textTheme.labelMedium,
               columns: [
                 DataColumn(label: Text('模型')),
+                // DataColumn(
+                //     label: Text(
+                //         GalleryLocalizations.of(context)!.contextWindow +
+                //             "\n(tokens)",
+                //         textAlign: TextAlign.center)),
                 DataColumn(
-                    label: Text(
-                        GalleryLocalizations.of(context)!.contextWindow +
-                            "\n(tokens)",
-                        textAlign: TextAlign.center)),
-                DataColumn(
-                    label: Text(
-                        GalleryLocalizations.of(context)!.price + "\n每百万Token",
-                        textAlign: TextAlign.center)),
+                    label: Column(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                      Text(GalleryLocalizations.of(context)!.price,
+                          textAlign: TextAlign.center),
+                      Text("每百万Token",
+                          style: Theme.of(context).textTheme.labelSmall),
+                    ])),
                 DataColumn(label: Text("综合评分"))
               ],
-              rows: [
-                DataRow(selected: true, cells: [
-                  DataCell(Text('GPT-4o mini')),
-                  DataCell(Text('128K')),
-                  DataCell(Text('输入: \$0.15\n输出: \$0.60')),
-                  DataCell(Text("41.26")),
-                ]),
-                DataRow(cells: [
-                  DataCell(Text('GPT-4o')),
-                  DataCell(Text('128K')),
-                  DataCell(Text('输入: \$2.50\n输出: \$10.00')),
-                  DataCell(Text("55.33")),
-                ]),
-                DataRow(cells: [
-                  DataCell(Text('o1 mini')),
-                  DataCell(Text('128K')),
-                  DataCell(Text('输入: \$1.10\n输出: \$4.40')),
-                  DataCell(Text("57.76")),
-                ]),
-                DataRow(cells: [
-                  DataCell(Text('DALL·E')),
-                  DataCell(Text('-')),
-                  DataCell(Text('\$0.04/image')),
-                  DataCell(Text("-")),
-                ]),
-                DataRow(selected: true, cells: [
-                  DataCell(Text('Claude 3.5 Haiku')),
-                  DataCell(Text('200K')),
-                  DataCell(Text('输入: \$0.80\n输出: \$4.00')),
-                  DataCell(Text("43.45")),
-                ]),
-                DataRow(cells: [
-                  DataCell(Text('Claude 3.5 Sonnet')),
-                  DataCell(Text('200K')),
-                  DataCell(Text('输入: \$3.00\n输出: \$15.00')),
-                  DataCell(Text("59.03")),
-                ]),
-                DataRow(cells: [
-                  DataCell(Text('Claude 3.7 Sonnet')),
-                  DataCell(Text('200K')),
-                  DataCell(Text('输入: \$3.00\n输出: \$15.00')),
-                  DataCell(_marddownSimple("65.56" + ":rocket:")),
-                ]),
-                DataRow(cells: [
-                  DataCell(Text('Claude 3 Opus')),
-                  DataCell(Text('200K')),
-                  DataCell(Text('输入: \$15.00\n输出: \$75.00')),
-                  DataCell(Text("49.16")),
-                ]),
-                DataRow(selected: true, cells: [
-                  DataCell(Text('Gemini 1.5 Pro')),
-                  DataCell(Text('2M')),
-                  DataCell(Text('输入: \$0.15\n输出: \$0.60')),
-                  DataCell(Text("54.33")),
-                ]),
-                DataRow(cells: [
-                  DataCell(Text('Gemini 2.0 Flash')),
-                  DataCell(Text('1M')),
-                  DataCell(Text('输入: \$0.10\n输出: \$0.40')),
-                  DataCell(_marddownSimple("61.47" + ":rocket:")),
-                ]),
-                DataRow(selected: true, cells: [
-                  DataCell(Text('DeepSeek V3')),
-                  DataCell(Text('64K')),
-                  DataCell(Text('输入: \$0.27\n输出: \$1.10')),
-                  DataCell(Text("60.45")),
-                ]),
-                DataRow(cells: [
-                  DataCell(Text('DeepSeek R1')),
-                  DataCell(Text('64K')),
-                  DataCell(Text('输入: \$0.55\n输出: \$2.19')),
-                  DataCell(_marddownSimple("71.57" + ":rocket:")),
-                ]),
-              ],
+              rows: Models.all.map((m) {
+                return DataRow(
+                  cells: [
+                    DataCell(Text(m.name)),
+                    DataCell(Text(
+                      '输入: \$${m.price["input"]}\n输出: \$${m.price["output"]}',
+                    )),
+                    DataCell(Text("${m.score}")),
+                  ],
+                );
+              }).toList(),
             ),
             _marddownSimple("评分数据来源于：[LiveBench](https://livebench.ai/#/)"),
           ],
